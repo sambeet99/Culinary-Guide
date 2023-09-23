429193e3a6371d25e2a91c15246be54547b53746
sambeet.nayak <sambeet.nayak@siemens.com>
2023-09-23 17:56:51 +0530

    add ingradients to recipe model and display them in each recipe details

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,20 @@
-export class Recipe{
-public name: String;
-public description: String;
-public imagePath: String;
+import { Ingradient } from '../shared/ingradient.model';
 
+export class Recipe {
+  public name: String;
+  public description: String;
+  public imagePath: String;
+  public ingradients: Ingradient[];
 
- constructor(name1: String, desc: String, path: String){
-
-    this.name=name1;
-    this.description=desc;
-    this.imagePath=path;
-}
+  constructor(
+    name1: String,
+    desc: String,
+    path: String,
+    ingradients: Ingradient[]
+  ) {
+    this.name = name1;
+    this.description = desc;
+    this.imagePath = path;
+    this.ingradients = ingradients;
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingradient } from '../shared/ingradient.model';
 
 export class RescipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -7,17 +8,30 @@ export class RescipeService {
     new Recipe(
       'pakhala',
       'khara dine',
-      'https://static.s123-cdn-static-c.com/uploads/2101965/2000_5e741e821a9ee.jpg'
+      'https://static.s123-cdn-static-c.com/uploads/2101965/2000_5e741e821a9ee.jpg',
+      [
+        new Ingradient('curd', 1),
+        new Ingradient('rice with water', 1),
+        new Ingradient('bhaja', 1),
+      ]
     ),
     new Recipe(
       'Abhada',
       'trip re',
-      'https://odishabytes.com/wp-content/uploads/2020/12/abhada-e1607415839585.jpg.webp'
+      'https://odishabytes.com/wp-content/uploads/2020/12/abhada-e1607415839585.jpg.webp',
+      [
+        new Ingradient('ticket to puri', 1),
+        new Ingradient('entry to mandir', 1),
+      ]
     ),
     new Recipe(
       'veg navratna',
       'feast',
-      'https://www.indianveggiedelight.com/wp-content/uploads/2021/04/navratan-korma-instant-pot.jpg'
+      'https://www.indianveggiedelight.com/wp-content/uploads/2021/04/navratan-korma-instant-pot.jpg',
+      [
+        new Ingradient('different veggies', 1),
+        new Ingradient('skill to cook', 1),
+      ]
     ),
   ];
 
